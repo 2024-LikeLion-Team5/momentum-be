45b5be1c053d37b91f6b2889ba1f4917b03de162
jeomxon <jeomxon@gmail.com>
2024-07-28 18:43:41 +0900

    feat: 리액트 로컬 CORS 허용

---
+++ src/main/java/com/momentum/global/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.momentum.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
