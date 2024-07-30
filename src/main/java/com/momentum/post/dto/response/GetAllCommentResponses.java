2140b0102ad5607d8af41aab32289d54245f11e2
jeomxon <jeomxon@gmail.com>
2024-07-31 08:46:25 +0900

    refactor: 특정 게시글 댓글 목록 조회 응답 필드 추가

--- src/main/java/com/momentum/post/dto/response/GetAllCommentResponses.java
+++ src/main/java/com/momentum/post/dto/response/GetAllCommentResponses.java
@@ -1,6 +1,8 @@
 package com.momentum.post.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.momentum.post.domain.Comment;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public record GetAllCommentResponses(
@@ -10,14 +12,17 @@ public record GetAllCommentResponses(
 
     public static GetAllCommentResponses of(long totalCommentCount, List<Comment> comments) {
         List<CommentResponse> commentResponses = comments.stream()
-                .map(comment -> new CommentResponse(comment.getId(), comment.getContent()))
+                .map(comment -> new CommentResponse(comment.getId(), comment.getContent(), comment.getCreatedAt()))
                 .toList();
         return new GetAllCommentResponses(totalCommentCount, commentResponses);
     }
 
     private record CommentResponse(
             long commentId,
-            String comment
+            String comment,
+
+            @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd.")
+            LocalDateTime createdAt
     ) {
     }
 }
